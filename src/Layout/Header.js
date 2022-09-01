977fe153dbe1c3e770636bcc9777417543b9def0
Marija Stankovic <marija.stankovic@work.co>
2022-09-01 10:48:41 +0200

    add react project

---
+++ src/Layout/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import HeaderNav from "../components/HeaderNav";
+
+function Header() {
+  return (
+    <div>
+      <p>logo</p>
+      <HeaderNav />
+      <button></button>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/components/HeaderNav.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import classes from "./HeaderNav";
+
+const HeaderNav = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default HeaderNav;
