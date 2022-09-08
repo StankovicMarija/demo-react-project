922266a5b8deaf1ec9ec2ca955d6e99c9266af1c
Marija Stankovic <marija.stankovic@work.co>
2022-09-09 00:09:42 +0200

    separated input comp

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,30 +1,29 @@
 import React from "react";
-import Mobile from "./mobile";
 import classes from "./hero.module.css";
 import Button from "../button/button";
 import Link from "../nav/link";
+import Input from "../input/input";
 
 const Hero = () => {
   return (
     <div className={classes.heroContainer}>
-      <Mobile />
+      <img src="../../img/mob.svg" alt="mobile" />
       <div className={classes.wrapper}>
         <h1 className={classes.caption}>
           Start building with our APIs for absolutely free.
         </h1>
         <div className={classes.sheduleForm}>
-          <input
-            className={classes.inputEmail}
-            type="text"
-            name="email"
-            placeholder="Enter email address"
-          />
+          <div className={classes.wrapperEmail}>
+            <Input />
+          </div>
           <div className={classes.formButton}>
             <Button>Schedule a Demo</Button>
           </div>
         </div>
         <h3 className={classes.text}>Have any questions?</h3>
-        <Link>Contact Us</Link>
+        <div className={classes.linkWrapper}>
+          <Link>Contact Us</Link>
+        </div>
       </div>
     </div>
   );
