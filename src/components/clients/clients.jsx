14fd74d70b18a9beab41a2ec1620deb53c36aabb
Marija Stankovic <marija.stankovic@work.co>
2022-09-16 11:28:39 +0200

    adding clients section

--- src/components/clients/clients.jsx
+++ src/components/clients/clients.jsx
@@ -2,38 +2,25 @@
 import React from 'react';
 import classes from './clients.module.css';
 import Button from '../button/button';
+import ClientsLogo from './clientsLogo/clientsLogo';
 
-const Clients = () => {
-  const clients = [
-    { name: 'tesla', src: '../../img/clients/tesla.svg' },
-    { name: 'mcrsft', src: '../../img/clients/mcrsft.svg' },
-    { name: 'hp', src: '../../img/clients/hp.svg' },
-    { name: 'oracle', src: '../../img/clients/oracle.svg' },
-    { name: 'google', src: '../../img/clients/google.svg' },
-  ];
-
-  return (
-    <div>
-      <div className={classes.clientsWrapper}>
-        {clients.map((client) => (
-          <img
-            key={client.name}
-            alt="client"
-            src={client.src}
-            className={classes.client}
-          />
-        ))}
-      </div>
-
-      <h2>Who we work with</h2>
-      <h3>
+const Clients = () => (
+  <div className={classes.clientsWrapper}>
+    <div className={classes.clientLogo}>
+      <ClientsLogo />
+    </div>
+    <div className={classes.clientTextWrapper}>
+      <h2 className={classes.caption}>Who we work with</h2>
+      <h3 className={classes.subtitle}>
         Today, millions of people around the world have successfully connected
         their accounts to apps they love using our API. We provide developers
         with the tools they need to create easy and accessible experiences for
         their users.
       </h3>
-      <Button>About Us</Button>
+      <div className={classes.btnWrapper}>
+        <Button variant="dark">About Us</Button>
+      </div>
     </div>
-  );
-};
+  </div>
+);
 export default Clients;
---
+++ src/components/clients/clientsLogo/clientsLogo.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import classes from './clientsLogo.module.css';
+import imgs from './clientLogoImages';
+
+const ClientsLogo = () => (
+  <>
+    {imgs.map((clientLogo) => (
+      <div key={clientLogo.name} className={classes.clientLogo}>
+        <img
+          src={clientLogo.src}
+          alt={clientLogo.name}
+        />
+      </div>
+    ))}
+  </>
+);
+
+export default ClientsLogo;
