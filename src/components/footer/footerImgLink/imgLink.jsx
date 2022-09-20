2d3bcdce892dabaf33f11c6b72619b58ae9a8268
Marija Stankovic <marija.stankovic@work.co>
2022-09-20 16:28:19 +0200

    adding link to footer component

--- src/components/footer/footerImgLink/imgLink.jsx
+++ src/components/footer/footerImgLink/imgLink.jsx
@@ -1,21 +1,23 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import classes from './imgLink.module.css';
 import { ReactComponent as FacebookLogo } from '../../../img/footer/f.svg';
 import { ReactComponent as TweeterLogo } from '../../../img/footer/tw.svg';
 import { ReactComponent as LinkedinLogo } from '../../../img/footer/ln.svg';
+import Link from '../../nav/link';
 
 function ImgLink() {
   return (
     <div>
-      <div className={classes.imgLink}>
+      <Link to="" className={classes.imgLink}>
         <FacebookLogo className={classes.imgLinkSvg} />
-      </div>
-      <div className={classes.imgLink}>
+      </Link>
+      <Link to="" className={classes.imgLink}>
         <TweeterLogo className={classes.imgLinkSvg} />
-      </div>
-      <div className={classes.imgLink}>
+      </Link>
+      <Link to="" className={classes.imgLink}>
         <LinkedinLogo className={classes.imgLinkSvg} />
-      </div>
+      </Link>
     </div>
   );
 }
